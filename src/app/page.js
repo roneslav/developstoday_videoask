e9320cc43ec9115f795c214460b7995722bd414f
roneslav <119205611+roneslav@users.noreply.github.com>
2025-01-10 20:04:45 +0200

    fix: add suspense component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import HomePage from './homePage/page';
+import Loader from './loader/Loader';
+
+const homePage = lazy(() => import('./homePage/page'));
 
 const Home = () => {
-  return <HomePage />;
+  return (
+    <Suspense fallback={<Loader />}>
+      <HomePage />
+    </Suspense>
+  );
 };
 
 export default Home;
